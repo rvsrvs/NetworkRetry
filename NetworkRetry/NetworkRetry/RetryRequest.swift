c9c58bf880a96819d81f9791ebbe544885189e08
Van Simmons <rvsrvs@gmail.com>
2015-11-06 10:52:10 -0500

    Updated comments
    Signed-off-by: Van Simmons <rvsrvs@gmail.com>

--- NetworkRetry/NetworkRetry/RetryRequest.swift
+++ NetworkRetry/NetworkRetry/RetryRequest.swift
@@ -15,35 +15,35 @@ import CFNetwork
 typealias AlamofireResponseCompletionHandler = (response:Alamofire.Response<AnyObject,NSError>) -> Void
 
 /*
- Retry wrapper method around Alamofire.  This method passes through to Alamofire and
+ Retry wrapper function around Alamofire.  This function passes through to Alamofire and
  in the event of a network down error, waits for the network to become available and
- retries the request one time only.
+ retries the request.  It will perform the retry one time only.
 
- In addition to the standard Alamofire request methods, this accepts:
+ In addition to the standard Alamofire request parameters which are passed through, this function accepts:
      retry:Bool to indicate whether or not to retry on CFURLErrorNotConnectedToInternet
      waitInterval:Double indicates the time to wait on the network to reappear
      completion:AlamofireResponseCompletionHandler to be called when we finally accept a result from Alamofire.
 
- This is implemented as a top level function in the manner of Alamofire's own calls
- The request object is returned immediately and can be queried
+ This function is implemented as a top level function in the manner of Alamofire's own calls
+ The request object created by Alamofire is returned immediately and can be queried
 */
 func requestJSON(method: Alamofire.Method, URLString: URLStringConvertible, parameters: [String: AnyObject]? = nil,
     retry:Bool = true, waitInterval:Double = 30.0, completion:AlamofireResponseCompletionHandler) -> Request
 {
-    // Make a standard Alamofire network request.  We expect to receive JSON back
+    // Make a standard Alamofire network request.  We expect to receive JSON back in the response
     let retVal = Alamofire.request(method, URLString, parameters: parameters).responseJSON {
         (response:Response) -> Void in
         // Handle the response from Alamofire.  This callback is invoked
         // After Alamofire has gone to the server, received a response and constructed
-        // JSON from the response.  Or received an error on the call
+        // JSON from the response.  Or received an error along the way
         switch response.result {
         case .Success:
-            // Just fall through to the original caller with success
+            // Just fall through to the original invoker with the response
             completion(response: response)
         case .Failure( let error ):
             // If we receive an error OTHER THAN CFURLErrorNotConnectedToInternet, then
-            // complete with an error because this function does not deal with other errors
-            // though other errors could be added if desired
+            // complete with that error because this function does not deal with other error types
+            // other errors could be added if desired, however
             if error.code != Int(CFNetworkErrors.CFURLErrorNotConnectedToInternet.rawValue) {
                 completion(response: response)
             }
@@ -51,23 +51,24 @@ func requestJSON(method: Alamofire.Method, URLString: URLStringConvertible, para
                 // The error encountered is due to Wifi being down.
                 
                 // If retry is false, this is fatal and we complete with
-                // the error response
+                // the error response.  This will happen if the network goes
+                // down again during our single retry attempt
                 if !retry {
                     completion(response: response)
                 }
                 else {
-                    // The network is down and retry is true so we will idle 
-                    // while waiting for Wifi to come back
+                    // Wifi is down and retry is true so we will idle
+                    // while waiting for it to come back
                     
-                    // We use Reachability to test for network availability
+                    // We use Reachability to test for Wifi availability
                     // Since we want to pause all user interaction
-                    // at this point we can check for the network
+                    // at this point we can check for Wifi availability
                     // inline rather than setting up all the notification
                     // mechanism and using callbacks
                     
                     // NB If we wanted the user to be able to do other tasks while
                     // waiting for this request to complete we would have to queue the request to
-                    // background and deal with multiple simultaneous actions at once.
+                    // background and deal with multiple potential simultaneous actions at once.
                     // This approach is much simpler but is applicable only for this
                     // sort of application.
                     
@@ -76,8 +77,9 @@ func requestJSON(method: Alamofire.Method, URLString: URLStringConvertible, para
                         // we will idle until a) the network is up or b) the waitInterval has been exceeded
                         if idleFor(waitInterval, orUntil: { wifiReach.isReachable() }) {
                             // the network came back - call ourself recursively
-                            // if the network goes away during this call, we will NOT try again
+                            // if the network goes away during this retry call, we will NOT try again
                             // this call will invoke the completion handler for success
+                            // hence we set retry to false here
                             requestJSON(method, URLString: URLString,
                                 parameters: parameters, retry: false, completion:completion)
                         }
@@ -105,10 +107,11 @@ func requestJSON(method: Alamofire.Method, URLString: URLStringConvertible, para
     Accepts:
     
     waitInterval:Double - how long to wait in seconds
-    orUntil:() -> Bool - a condition which if met immediately terminates the func
+    orUntil:() -> Bool - a condition which, if met (i.e. true), immediately terminates the func
+    and returns true
 
     orUntil defaults to: { return false } which ensures that the method
-    will wait until the waitInterval is exceeded
+    will wait until the waitInterval is exceeded if orUntil is not passed in
 
     returns true if the orUntil condition was met, false if waitInterval was reached
 
